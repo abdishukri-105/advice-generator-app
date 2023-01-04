68ea13f8e5226e9cae0f06130461798f8ccfc227
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-04 21:23:14 +0300

    succefully fetched data from api

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(response => response.json())
       .then(data => {
         console.log(data)
+        renderAdvice(data);
       } )
+    
+
+      const renderAdvice = (data) =>{
+           const message = document.querySelector("#adviceContent")
+           message.innerHTML = data.slip.advice;
+           console.log(message)
+           
+           const adviceId = document.querySelector("#adviceNo")
+           adviceId.innerHTML = data.slip.id
+      }
+
 })
